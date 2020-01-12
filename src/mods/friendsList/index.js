1c7799bf268b89f2bc1d10f4c56905b64f8cd9fa
Cullen <cullenmclaren@gmail.com>
2020-01-12 16:00:25 -0500

    Add tooltip for Friends List & XP Meter
    - Move all custom mod nav buttons to the right
    - Add ui.createNavButton method

--- src/mods/friendsList/index.js
+++ src/mods/friendsList/index.js
@@ -1,19 +1,14 @@
-import { makeElement } from '../../utils/misc';
-import { createFriendsList, toggleFriendsList, isWindowOpen, WindowNames } from '../../utils/ui';
+import {
+	createFriendsList,
+	toggleFriendsList,
+	isWindowOpen,
+	WindowNames,
+	createNavButton,
+} from '../../utils/ui';
 
 // The F icon and the UI that appears when you click it
 function customFriendsList() {
-	var friendsIconElement = makeElement({
-		element: 'div',
-		class: 'btn border black js-friends-list-icon',
-		content: 'F',
-	});
-	// Add the icon to the right of Elixir icon
-	const $elixirIcon = document.querySelector('#sysgem');
-	$elixirIcon.parentNode.insertBefore(friendsIconElement, $elixirIcon.nextSibling);
-
-	// Create the friends list UI
-	document.querySelector('.js-friends-list-icon').addEventListener('click', toggleFriendsList);
+	createNavButton('friendslist', 'F', 'Friends List', toggleFriendsList);
 
 	// If it was open when the game last closed keep it open
 	if (isWindowOpen(WindowNames.friendsList)) {
